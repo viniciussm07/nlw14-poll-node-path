6b850a5c44a6abc26214ad36b669a355994b22fa
viniciussm07 <vini.soares.martins@gmail.com>
2024-02-08 10:51:47 -0300

    Aula 3

--- src/http/routes/vote-on-poll.ts
+++ src/http/routes/vote-on-poll.ts
@@ -2,6 +2,8 @@ import { FastifyInstance } from "fastify";
 import { randomUUID } from "node:crypto";
 import { prisma } from "../../lib/prisma";
 import { z } from "zod";
+import { redis } from "../../lib/redis";
+import { voting } from "../../utils/voting-pub-sub";
 
 export async function voteOnPoll(app: FastifyInstance) {
   app.post("/polls/:pollId/votes", async (request, reply) => {
@@ -32,13 +34,18 @@ export async function voteOnPoll(app: FastifyInstance) {
         userPreviousVoteOnPoll &&
         userPreviousVoteOnPoll.pollOptionId !== pollOptionId
       ) {
-        // Apagar o voto anterior
-        // Criar um novo
         await prisma.vote.delete({
           where: {
             id: userPreviousVoteOnPoll.id,
           },
         });
+        const votes = await redis.zincrby(pollId, -1, userPreviousVoteOnPoll.pollOptionId);
+
+        voting.publish(pollId, {
+          pollOptionId: userPreviousVoteOnPoll.pollOptionId,
+          votes: Number(votes),
+        });
+
       } else if (userPreviousVoteOnPoll) {
         return reply
           .status(400)
@@ -65,6 +72,13 @@ export async function voteOnPoll(app: FastifyInstance) {
       },
     });
 
-    return reply.status(201).send({sessionId});
+    const votes = await redis.zincrby(pollId, 1, pollOptionId);
+
+    voting.publish(pollId, {
+      pollOptionId,
+      votes: Number(votes),
+    });
+
+    return reply.status(201).send();
   });
 }
